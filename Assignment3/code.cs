8688168e4f3b65459b8e1e00ad12e2e648bcfdd5
garima24797 <84575081+garima24797@users.noreply.github.com>
2021-07-28 16:00:27 +0530

    Update code.cs

--- Assignment3/code.cs
+++ Assignment3/code.cs
@@ -1,1 +1,24 @@
+Void FixedUpdate()
+    {
+        Var deltaPosition = Vector3.zero;
+        For (int I = 0; I < NumberOfRays; i++)
+        {
+            
+            Var rotation = this.transform.rotation;
+           Var RotationMod= Quaternion.AngleAxis((I /((float)NumberOfRays-1)) * angle*2-angle, this.transform.up);
+            Var direction = rotation * RotationMod * Vector3.forward;
+            RaycastHit hitInfo;
+            Var ray = new Ray(this.transform.position, direction);
+            If (Physics.Raycast(ray, out hitInfo, ray_range)) 
+            {
+                deltaPosition -= (1.0f / NumberOfRays) * TargetVelocity * direction;
+            }
+            Else
+            {
+                deltaPosition += (1.0f / NumberOfRays) * TargetVelocity * direction;
+            }
+        }
 
+        This.transform.position += deltaPosition * Time.deltaTime;
+
+    }
